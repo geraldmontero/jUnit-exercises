316d2ce19d311923cd58e24b6bccf84d1858e1be
Gerald Montero <geraldmontero12@gmail.com>
2022-03-14 12:28:34 -0500

    update

---
+++ src/test/java/junitTests.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class junitTests {
+
+    @Test
+    public void testIfNameIsKenneth() {
+        String expected = "Kenneth";
+        String actual = "Kenneth";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestIfChange(){
+        Double price = 10.1;
+        Double discount = 4.5;
+
+        assertEquals(5.1,price-discount,.5);
+        assertNotEquals(4.2,price-discount,.5);
+
+    }
+
+}
